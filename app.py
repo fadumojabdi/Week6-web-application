81b644ea33f46a9c0f45223415d0d35f4b917a65
Fadumo Jama <>
2024-02-21 15:11:18 +0000

    first commit

--- app.py
+++ app.py
@@ -5,6 +5,43 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # == Your Routes Here ==
+@app.route('/hello', methods=['GET'])
+def hello():
+    name = request.args['name']
+    return  f"Hello {name}!"
+
+@app.route('/goodbye', methods=['POST'])
+def goodbye():
+    name = request.form['name']
+    return  f"Goodbye {name}!"
+
+
+# == EXERCISE 1 ==
+#Create a new route that responds to A method   POST
+# A path /submit
+# Body parameters name and message
+
+@app.route('/submit', methods=['POST'])
+def post_submit():
+    name = request.form['name']
+    message = request.form['message']
+    return  f"Thank you {name} for your message: {message}!"
+
+# run in the terminal with the following command: curl -X POST -d "name=Leo&message=Hello world" http://localhost:5001/submit
+#for my local host is 5001.
+
+# == CHALLENGE ==
+# Create a new route that responds to A method   GET
+# A path /wave
+# Query parameters name
+# The route should return a string that says "I am waving at [NAME]" followed by the name in the query parameter.
+
+@app.route('/wave', methods=['GET'])
+def get_wave():
+    name = request.args['name']
+    return  f"I am waving at {name}!"
+
+# run in the terminal with the following command: curl http://localhost:5001/wave?name=Leo
 
 # == Example Code Below ==
 
